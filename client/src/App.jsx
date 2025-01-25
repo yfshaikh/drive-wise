55ff32624a0ac185ae21669e7d3ddc6cd22c52d8
taahahussain <taahahussain12204@gmail.com>
2025-01-25 17:51:18 -0600

    quiz

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 
 // PAGES
 import LandingPage from "./pages/LandingPage"
-
+import UserInfoQuiz from "./pages/UserInfoQuiz"
 function App() {
 
   return (
@@ -11,6 +11,7 @@ function App() {
         <div className="app">
           <Routes>
             <Route path = '/' element={<LandingPage />} />
+            <Route path = '/quiz' element={<UserInfoQuiz />} />
           </Routes>
         </div>
       </Router>
---
+++ client/src/components/index.js
@@ -0,0 +1,2 @@
+export { default as SingleChoiceQuestion } from './SingleChoiceQuestion';
+export { default as MultiChoiceQuestion } from './MultiChoiceQuestion';
